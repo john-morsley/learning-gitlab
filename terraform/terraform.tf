5986547bef44fdd24c54caccf1c7ee9173d6a1bf
John Morsley <johnmorsley@gmail.com>
2020-11-13 01:23:05 +0000

    .

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,9 +1,20 @@
 terraform {
-    backend "remote" {
-        hostname = "app.terraform.io"
-        organization = "john-morsley"
-        workspaces {
-            name = "learning-gitlab" 
-        }
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.0"
     }
+  }
+
+  required_version = ">= 0.13"
+  
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "john-morsley"
+    workspaces {
+      name = "learning-gitlab"
+    }
+  }
+  
 }
